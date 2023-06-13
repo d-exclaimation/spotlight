cf0fe84184022e17bd2dca7373d1656df18f1225
Vincent <thisoneis4business@gmail.com>
2023-06-13 21:04:16 +1200

    fix: Fixed env

--- src/routes/test/[test]/+server.ts
+++ src/routes/test/[test]/+server.ts
@@ -2,6 +2,8 @@ import { PRIVATE_ALLOWED_EMAILS } from "$env/static/private";
 import { json } from "@sveltejs/kit";
 
 export function GET({ params: { test } }) {
-  const allowed = PRIVATE_ALLOWED_EMAILS.includes(test.toLowerCase());
+  const allowed = PRIVATE_ALLOWED_EMAILS.split(",").includes(
+    test.toLowerCase()
+  );
   return json({ allowed });
 }

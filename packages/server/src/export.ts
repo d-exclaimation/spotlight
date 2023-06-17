737d08ddfa324127209d437a88bf8bb330c41cf7
Vincent <thisoneis4business@gmail.com>
2023-06-17 20:34:55 +1200

    feat: expose input and output types

--- packages/server/src/export.ts
+++ packages/server/src/export.ts
@@ -1,1 +1,7 @@
+import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import { App } from "./trpc/index.js";
+
+export type AppInput = inferRouterInputs<App>;
+export type AppOutput = inferRouterOutputs<App>;
+
 export type { App } from "./trpc/index.js";

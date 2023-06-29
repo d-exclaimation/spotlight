7e44ba274f07405856b85a714c05bafead576965
Vincent <thisoneis4business@gmail.com>
2023-06-30 02:22:31 +1200

    feat: edge > serverless

--- packages/web/src/routes/internal/login/+server.ts
+++ packages/web/src/routes/internal/login/+server.ts
@@ -1,5 +1,10 @@
 import { setProxyToken } from "@/lib/server/cookie.js";
+import type { Config } from "@sveltejs/adapter-vercel";
 import { json } from "@sveltejs/kit";
+
+export const config = {
+  runtime: "edge",
+} satisfies Config;
 
 export function POST(event) {
   const [kind, token] =
--- packages/web/src/routes/internal/logout/+server.ts
+++ packages/web/src/routes/internal/logout/+server.ts
@@ -1,5 +1,10 @@
 import { clearProxyToken } from "@/lib/server/cookie.js";
+import type { Config } from "@sveltejs/adapter-vercel";
 import { json } from "@sveltejs/kit";
+
+export const config = {
+  runtime: "edge",
+} satisfies Config;
 
 export async function POST(event) {
   clearProxyToken(event);

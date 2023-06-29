a12e9596ca0094a6c74c1fcfead770443745352a
Vincent <thisoneis4business@gmail.com>
2023-06-30 02:30:08 +1200

    fix: Cookie expired immediatelly

--- packages/web/src/lib/server/cookie.ts
+++ packages/web/src/lib/server/cookie.ts
@@ -4,7 +4,12 @@ export function setProxyToken<T extends RequestEvent<any, any>>(
   event: T,
   value: string
 ) {
-  event.cookies.set("proxy-token", value);
+  event.cookies.set("proxy-token", value, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: true,
+    expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365), // 1 year
+  });
 }
 
 export function getProxyToken<T extends RequestEvent<any, any>>(event: T) {

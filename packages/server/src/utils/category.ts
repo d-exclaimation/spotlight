fab7fbf21cd9b8f9dd2ef6ccc78216ecb7cd90ed
Vincent <thisoneis4business@gmail.com>
2023-07-03 19:45:27 +1200

    feat: Category is made from spotlight/ai

--- packages/server/src/utils/category.ts
+++ packages/server/src/utils/category.ts
@@ -1,17 +1,1 @@
-import { z } from "zod";
-/**
- * The category of the news.
- */
-export type Category = z.infer<typeof Category>;
-export const Category = z.nativeEnum({
-  tech: "tech",
-  ai: "ai",
-  politic: "politic",
-  history: "history",
-  jobs: "jobs",
-  uncategorized: "uncategorized",
-  world: "world",
-  finance: "finance",
-  science: "science",
-  sport: "sport",
-} as const);
+export { Category } from "@spotlight/ai";
